f69d6876188285f27dfa0bbb8ba793ccfc73d71f
davidunilab <davit.chincharashvili.1@iliauni.edu.ge>
2021-03-21 14:27:20 +0400

    added convert to radians

--- main.py
+++ main.py
@@ -1,39 +1,17 @@
-import matplotlib.pyplot as plt
-class Book:
-    def __init__(self, name, pages, current_page):
-        self.name = name
-        self.pages = pages
-        self.current_page = current_page
-        self.bookmarks = []
+import math
+from fractions import Fraction
+from flask import Flask
 
-    def progress(self):
-        return self.current_page / self.pages * 100
+app = Flask(__name__)
 
-    def pages_left(self):
-        return self.pages - self.current_page
+@app.route('/')
+def home():
+    return "to_radians/number"
 
-    def bookmark(self, page):
-        self.bookmarks.append(page)
+@app.route('/to_radians/<int:x>')
+def toRadians(x):
+    integer = math.radians(x)
+    return f"{Fraction.from_float(integer).limit_denominator(10)} იგივე რაც {integer} რადიანი"
 
-    def average_reading_speed(self):
-        avg = sum(self.bookmarks) / len(self.bookmarks)
-        plt.plot(self.bookmarks, marker="o")
-        plt.title(f"კითხვის საშუალო სიჩქარეა - {avg}")
-        plt.xlabel("დღეები")
-        plt.ylabel("გვერდები")
-        plt.grid()
-        plt.show()
-        return avg
-
-
-shta = Book("შთა", 454, 39)
-shta.bookmark(23)
-shta.bookmark(45)
-shta.bookmark(58)
-shta.bookmark(123)
-shta.bookmark(145)
-shta.bookmark(167)
-shta.bookmark(190)
-shta.bookmark(200)
-print(shta.average_reading_speed())
-
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
